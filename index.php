b2767e3095cc4c5f36c2ddeb23a30d4c807afe38
ilhamsyamsuddin <ilham5442@gmail.com>
2020-02-06 13:48:35 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+
+        <title>Coba Git Remote</title>
+        
+    </head> 
+
+    <body>
+        <h1>Hello world</h1>
+        <p>lorem ipsum</p>
+    </body>
+</html>
